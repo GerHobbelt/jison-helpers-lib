b9ee2413b1c2fe2f6b34cc13d96ad3c805fc071d
Ger Hobbelt <ger@hobbelt.com>
2017-09-29 17:58:02 +0200

    initialized the library repo with all mandatory files.

---
+++ tests/tests.js
@@ -0,0 +1,11 @@
+var assert = require("chai").assert;
+var helpers = require('../index.js');
+
+// TODO real tests
+
+describe("helpers API", function () {
+  it("TBD", function () {
+    assert.equal(1, 1);
+  });
+});
+
